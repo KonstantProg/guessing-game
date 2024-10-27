8ece93b3f87de196745f9f3a27d025fdc946e09f
Konstantin Ahremchuk <ahremchuk.kostya@gmail.com>
2024-10-27 14:27:10 +0300

    solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.maxValue = max;
+        this.minValue = min;
     }
 
     guess() {
-
+        this.result = Math.round((this.maxValue + this.minValue) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.maxValue = this.result;
     }
 
     greater() {
-
+        this.minValue = this.result;
     }
 }
 
